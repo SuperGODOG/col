365344639081e7325fc2c61fc0fb0dd346034da6
crx <crx031214@gmail.com>
2023-04-21 14:55:50 +0800

    Initial commit

---
+++ tesk.h
@@ -0,0 +1,31 @@
+//
+// Created by 余顺萍 on 2023/4/19.
+//
+
+#ifndef UNTITLED_TESK_H
+#define UNTITLED_TESK_H
+
+
+class tesk {
+
+};
+class abc
+{
+private:
+    int x;
+    char y[10];
+public:
+    void demo1();
+    void demo1();
+};
+class def : private abc
+{
+private:
+    int p;
+    char q[10];
+public:
+    void demo3();
+    void demo4();
+};
+
+#endif //UNTITLED_TESK_H
